d8015d5b3fbba9d958537b39a1db65b223f00d80
vdmst <v.sotov.webdev@yandex.ru>
2026-04-21 14:54:34 +0300

    Update createRouter.tsx

--- src/app/router/config/createRouter.tsx
+++ src/app/router/config/createRouter.tsx
@@ -1,6 +1,4 @@
-import { createBrowserRouter } from 'react-router-dom'
+import { createHashRouter } from 'react-router-dom'
 import { routes } from './routes'
 
-export const router = createBrowserRouter(routes, {
-    basename: import.meta.env.BASE_URL,
-})
+export const router = createHashRouter(routes)
