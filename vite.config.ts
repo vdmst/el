ca4c082393303e5075e48a8cd311bf045a3168a8
vdmst <v.sotov.webdev@yandex.ru>
2026-04-21 15:00:01 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,25 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
 import svgr from 'vite-plugin-svgr'
 
 // https://vite.dev/config/
-export default defineConfig(({ mode }) => ({
-    base: mode === 'production' ? '/el/' : '/',
-    resolve: {
-        alias: {
-            '@': path.resolve(__dirname, './src'),
+export default defineConfig(({ mode }) => {
+    const env = loadEnv(mode, process.cwd(), '')
+
+    return {
+        base: mode === 'production' ? env.VITE_BASE_PATH || '/el/' : '/',
+        resolve: {
+            alias: {
+                '@': path.resolve(__dirname, './src'),
+            },
         },
-    },
-    plugins: [react(), svgr()],
-    css: {
-        postcss: './postcss.config.js',
-    },
-    server: {
-        port: 3000,
-    },
-}))
+        plugins: [react(), svgr()],
+        css: {
+            postcss: './postcss.config.js',
+        },
+        server: {
+            port: 3000,
+        },
+    }
+})
